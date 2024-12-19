dc98f70f6dd0fbcdea440405f07e43a14b9e7350
Mischa Lamoureux <mischalamoureux7@gmail.com>
2024-12-18 20:06:21 -0700

    Added ingestion for sport, event, and result

--- ingest_csv_data.sql
+++ ingest_csv_data.sql
@@ -9,3 +9,15 @@ WITH (FORMAT csv, DELIMITER ',', HEADER true);
 COPY game(game_id, title, city, start_date, end_date, was_held, country_id)
 FROM '/Users/mischa/Library/Mobile Documents/com~apple~CloudDocs/University of Waterloo/Classes/3B/Databases/Assignments/Group Project/databases_group_k/dataset/game.csv'
 WITH (FORMAT csv, DELIMITER ',', HEADER true);
+
+COPY sport(sport_id, name)
+FROM '/Users/mischa/Library/Mobile Documents/com~apple~CloudDocs/University of Waterloo/Classes/3B/Databases/Assignments/Group Project/databases_group_k/dataset/sport.csv'
+WITH (FORMAT csv, DELIMITER ',', HEADER true);
+
+COPY "event"(event_id, name, gender, is_team_event, sport_id)
+FROM '/Users/mischa/Library/Mobile Documents/com~apple~CloudDocs/University of Waterloo/Classes/3B/Databases/Assignments/Group Project/databases_group_k/dataset/event.csv'
+WITH (FORMAT csv, DELIMITER ',', HEADER true);
+
+COPY "result"(result_id, "position", game_id, event_id, athlete_id)
+FROM '/Users/mischa/Library/Mobile Documents/com~apple~CloudDocs/University of Waterloo/Classes/3B/Databases/Assignments/Group Project/databases_group_k/dataset/result.csv'
+WITH (FORMAT csv, DELIMITER ',', HEADER true);
